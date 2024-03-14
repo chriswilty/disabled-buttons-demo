9058240e5cfeb89b0d10d9fbe95b781b7bcbf48f
Chris Wilton-Magras <chriswilty@zoho.com>
2024-03-14 10:46:58 +0000

    Change button labels to be more descriptive

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
+	const downIsDisabled = count <= 0;
+
 	const increment = () => setCount((prev) => prev + 1);
 	const decrement = () => setCount((prev) => Math.max(prev - 1, 0));
-
-	const downIsDisabled = count <= 0;
 
 	return (
 		<main>
@@ -25,19 +25,19 @@ function App() {
 					className="boxed"
 					onClick={decrement}
 				>
-					Down
-					<span className="arrow" aria-hidden="true">
-						&#8675;
+					Minus
+					<span className="icon" aria-hidden="true">
+						-
 					</span>
 				</button>
 				<div role="tooltip" id="why-disabled" className="tooltip">
 					<span>Value cannot be negative</span>
 				</div>
 				<button className="boxed" onClick={increment}>
-					<span className="arrow" aria-hidden="true">
-						&#8673;
+					<span className="icon" aria-hidden="true">
+						+
 					</span>
-					Up
+					Plus
 				</button>
 			</div>
 		</main>
